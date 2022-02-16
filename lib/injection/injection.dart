af2007cabff5b0d6fb493e716573a00e5edfae0b
pedroenrre <pedro_enrre@hotmail.com>
2022-02-16 19:15:51 -0300

    adds getIt to make injections

---
+++ lib/injection/injection.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pokedex/modules/home/home_controller.dart';
+import 'package:pokedex/modules/home/repository/home_repository.dart';
+import 'package:pokedex/modules/home/store/home_store.dart';
+
+Future<void> configureDependencies() async {
+  GetIt.I.registerFactory(() => Dio());
+  GetIt.I.registerFactory(() => HomeStore());
+  GetIt.I.registerFactory(() => HomeRepository(GetIt.I.get()));
+  GetIt.I.registerFactory(() => HomeController(GetIt.I.get(), GetIt.I.get()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'app/app.dart';
+import 'injection/injection.dart';
 
-void main() {
+void main() async {
+  await configureDependencies();
   runApp(const MyApp());
 }
